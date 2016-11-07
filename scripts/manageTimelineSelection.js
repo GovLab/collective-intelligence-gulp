16d62ca561afe21e6d147ad55d3e44e5b6232e55
Ena Bek <enabek@gmail.com>
2016-11-07 11:37:48 -0500

    Update 2016-11-07T16:37:47.468Z

---
+++ scripts/manageTimelineSelection.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var e=$(".e-event"),t=$(".e-timebox"),n=$(".b-timeline-list"),a=!0;e.each(function(e){var i=new Date($(this).find("time").attr("datetime"));(a&&i<=$.now()||a&&i===$.now())&&($(this).addClass("m-current"),$(t.get(e)).addClass("m-current"),$(n.children().get(e)).addClass("m-current"),a=!1)})});
