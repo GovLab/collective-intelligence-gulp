47c5a1b0588665bf35cf2b06d690bc14996cc164
annabialas <ab5086@nyu.edu>
2016-12-05 15:18:38 -0500

    Update 2016-12-05T20:18:38.109Z

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,34 +1,4 @@
-$(document).ready(function () {
-    // Smooth Scrolling Function
-    // $('a[href*=#]:not([href=#])').click(function () {
-    //     var $targ = $(this.hash),
-    //         host1 = this.hostname,
-    //         host2 = location.hostname,
-    //         path1 = this.pathname.replace(/^\//, ''),
-    //         path2 = location.pathname.replace(/^\//, '');
-
-    //     if (!$targ.length) {
-    //         $targ = $('[name=' + this.hash.slice(1) + ']');
-    //     }
-
-    //     if ($targ.length && (host1 === host2 || path1 === path2)) {
-    //         $('html, body').animate({ scrollTop: $targ.offset().top }, 1000);
-
-    //         return false;
-    //     }
-
-    //     return true;
-    // });
-
-    // Modal Click Behavior
-    $('.js-open-modal').click(function () {
-        $('.js-target-modal').addClass('js-active');
-        $('#overlay').addClass('js-active');
-        $('body').addClass('js-body-modal-active');
-    });
-
-    $('.js-close-modal').click(function () {
-        $('.js-target-modal').removeClass('js-active');
+$(document).ready(function(){function e(){a.fadeIn(2e3).delay(2500).fadeOut(2e3,function(){e()}).html(s[c++]),c==s.length&&(c=0)}$(".js-open-modal").click(function(){$(".js-target-modal").addClass("js-active"),$("#overlay").addClass("js-active"),$("body").addClass("js-body-modal-active")}),$(".js-close-modal").click(function(){$(".js-target-modal").removeClass("js-active"),$("#overlay").removeClass("js-active"),$("body").removeClass("js-body-modal-active")}),$(".js-close-sticky").click(function(){$(".js-target-sticky").removeClass("js-active")}),$(".js-trigger-search").click(function(e){e.preventDefault(),$(this).parent().addClass("js-active"),$("#overlay").addClass("js-active")}),$(".js-trigger-menu").click(function(e){$(this).next().addClass("js-active-menu"),$("#overlay").addClass("js-active")}),$("#overlay").click(function(){$(".js-active").removeClass("js-active"),$(".js-active-menu").removeClass("js-active-menu")});var a=$(".collective-banner").hide(),s=$(".collective-banner").children(),c=0;e()});s-active');
         $('#overlay').removeClass('js-active');
         $('body').removeClass('js-body-modal-active');
     });
